bdf8c5c30201509d4b4fcd82da7ae0990ca74e39
Hunter <Hunter@LAPTOP-494ORLEL>
2017-11-01 17:59:53 +0800

    Revert "Revert "no message""
    This reverts commit 3210237e9a056c1dcbcfbf31acd05de1203afbbb.

---
+++ PM/PM.Application/MultiTenancy/Dto/EditTenantDto_BACKUP_4928.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.AutoMapper;
+using PM.Core.MultiTenant;
+
+namespace PM.Application.MultiTenancy.Dto
+{
+    [AutoMapTo(typeof (Tenant))]
+    public class EditTenantDto
+    {
+        [Required]
+        [StringLength(Tenant.MaxTenancyNameLength)]
+        public string TenancyName { get; set; }
+
+        [Required]
+        [StringLength(Tenant.MaxNameLength)]
+        public string Name { get; set; }
+<<<<<<< HEAD
+
+        public bool IsActive { get; set; }
+=======
+>>>>>>> 75f0b478152396ef7b98500e7af4e14492a2e193
+
+        public bool IsActive { get; set; }
+    }
+}
---
+++ PM/PM.Application/Roles/Dto/CreateRoleDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.AutoMapper;
+using PM.Core.Authorization.Roles;
+
+namespace PM.Application.Roles.Dto
+{
+    [AutoMapTo(typeof(Role))]
+    public class CreateRoleDto
+    {
+        [Required]
+        [StringLength(Role.MaxNameLength)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(Role.MaxDisplayNameLength)]
+        public string DisplayName { get; set; }
+        public string NormalizedName { get; set; }
+
+        [StringLength(Role.MaxDescriptionLength)]
+        public string Description { get; set; }
+
+        public bool IsStatic { get; set; }
+
+        public List<string> Permissions { get; set; } 
+
+    }
+}
