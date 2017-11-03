8ecb5a16a6934d0e42d067b1c03f14bfbe9f7e46
Hunter <Hunter@LAPTOP-494ORLEL>
2017-11-03 16:56:40 +0800

    no message

--- PM/PM.Application/PMApplicationModule.cs
+++ PM/PM.Application/PMApplicationModule.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Abp.Authorization;
+using Abp.Authorization.Roles;
 using Abp.AutoMapper;
 using Abp.Modules;
+using PM.Application.Roles.Dto;
+using PM.Application.Users.Dto;
 using PM.Core;
+using PM.Core.Authorization.Roles;
+using PM.Core.Users;
 
 namespace PM.Application
 {
@@ -16,8 +22,21 @@ namespace PM.Application
         public override void Initialize()
         {
             IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
-            //TODO: 
+            Configuration.Modules.AbpAutoMapper().Configurators.Add(cfg =>
+            {
+                // Role and permission
+                cfg.CreateMap<Permission, string>().ConvertUsing(r => r.Name);
+                cfg.CreateMap<RolePermissionSetting, string>().ConvertUsing(r => r.Name);
 
+                cfg.CreateMap<CreateRoleDto, Role>().ForMember(x => x.Permissions, opt => opt.Ignore());
+                cfg.CreateMap<RoleDto, Role>().ForMember(x => x.Permissions, opt => opt.Ignore());
+
+                cfg.CreateMap<UserDto, User>();
+                cfg.CreateMap<UserDto, User>().ForMember(x => x.Roles, opt => opt.Ignore());
+
+                cfg.CreateMap<CreateUserDto, User>();
+                cfg.CreateMap<CreateUserDto, User>().ForMember(x => x.Roles, opt => opt.Ignore());
+            });
 
         }
     }
--- PM/PM.Test/PMTestModule.cs
+++ PM/PM.Test/PMTestModule.cs
@@ -4,7 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Abp.Modules;
+using Abp.MultiTenancy;
 using Abp.TestBase;
+using Abp.Zero.Configuration;
+using Castle.MicroKernel.Registration;
+using NSubstitute;
 using PM.Application;
 using PM.EntityFramework;
 
@@ -17,5 +21,18 @@ namespace PM.Test
        )]
     public class PMTestModule:AbpModule
     {
+        public override void PreInitialize()
+        {
+            //使用数据库进行语言管理
+            Configuration.Modules.Zero().LanguageManagement.EnableDbLocalization();
+
+            //注册伪服务
+
+            IocManager.IocContainer.Register(
+                Component.For<IAbpZeroDbMigrator>()
+                    .UsingFactoryMethod(() => Substitute.For<IAbpZeroDbMigrator>())
+                    .LifestyleSingleton()
+                );
+        }
     }
 }
